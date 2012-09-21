d04f0bb160f2806048d7adfcb2016356a4a01f63
mathieu <mathieucans@gmail.com>
2012-09-21 22:13:52 +0200

    first commits

---
+++ TDDice.Test/MoreThan30LessThan12GameTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TDDice.Test
+{
+    [TestFixture]
+    public class MoreThan30LessThan12GameTest
+    {
+        [TestCase]
+        public void PlayZone_AtInit_ShouldContains6Elements()
+        {
+            var game = new MoreThan30LessThan12Game();
+
+            Assert.AreEqual(6, game.PlayPanel.Count());
+        }
+
+        [TestCase]
+        public void SavedZone_AtInit_ShouldContains0Elements()
+        {
+            var game = new MoreThan30LessThan12Game();
+
+            Assert.AreEqual(0, game.SavedPanel.Count());
+        }
+
+        [TestCase]
+        public void SaveDice_WithFirstPlayedDice_ShoudAddDiceToTheSavedPanel()
+        {
+            var game = new MoreThan30LessThan12Game();
+            var dice = game.PlayPanel.First();
+
+            game.SaveDice(dice);
+
+            CollectionAssert.Contains(game.SavedPanel, dice);
+        }
+
+    }
+}
---
+++ TDDice/MoreThan30LessThan12Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace TDDice
+{
+    public class MoreThan30LessThan12Game
+    {
+        private IList<object> _savedPanel;
+
+        public IEnumerable<object> PlayPanel { get; private set; }
+
+        public IEnumerable<object> SavedPanel { get { return _savedPanel;} }
+
+        public MoreThan30LessThan12Game()
+        {
+            PlayPanel = new object[6];
+            _savedPanel = new object[0];
+        }
+
+        public void SaveDice(object dice)
+        {
+            _savedPanel.Add(dice);
+        }
+    }
+}
